b0df12d9939b7a21ab8649844c6808cf52257d7e
Johnny Cao <jcao0928@gmail.com>
2020-12-27 07:00:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,20 @@ end
 def using_unshift (new_array, new_string)
   new_array.unshift (new_string)
 end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
